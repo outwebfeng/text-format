745eb8fef50e7b7aee032300625190b010eb2c7e
outwebfeng <amylindsey958@gmail.com>
2024-07-01 01:43:39 +0800

    fix bug

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -14,7 +14,7 @@ export default function NotFound() {
         <div className="text-center">
           <p className="text-base font-semibold text-[#ffa11b]">404</p>
           <h1 className="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">Page not found</h1>
-          <p className="mt-6 text-base leading-7 text-gray-600">Sorry, we couldn't find the page you're looking for.</p>
+          <p className="mt-6 text-base leading-7 text-gray-600">Sorry, we couldn’t find the page you’re looking for.</p>
           <div className="mt-10 flex items-center justify-center gap-x-6">
             <a href="https://text-format.com" className="rounded-md bg-[#ffa11b] px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-[#f05011] focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Go back home</a>
           </div>
