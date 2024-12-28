b1214e1c854616ae1a29c49b68cedcda57c6951f
outwebfeng <amylindsey958@gmail.com>
2024-12-28 17:27:10 +0800

    处理vercel worker 报错问题。

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,25 @@ import createNextIntlPlugin from 'next-intl/plugin';
 const withNextIntl = createNextIntlPlugin();
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config, { isServer }) => {
+    // 添加 worker-loader 配置
+    config.module.rules.push({
+      test: /\.worker\.js$/,
+      loader: 'worker-loader',
+      options: {
+        filename: 'static/[hash].worker.js',
+        publicPath: '/_next/',
+      },
+    });
+
+    // 避免 worker 相关的 SSR 错误
+    if (isServer) {
+      config.output.globalObject = 'self';
+    }
+
+    return config;
+  },
+};
 
 export default withNextIntl(nextConfig);
